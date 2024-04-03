24089535c3ce338a3e74fdb64f77175e2c5e4c89
trbouma <trbouma@gmail.com>
2024-04-03 12:06:42 -0400

    edits

--- supername/main.py
+++ supername/main.py
@@ -2,7 +2,11 @@
 
 import click
 
-# from lib.simple_package import math_operations
+if __name__ == '__main__':
+    from lib.simple_package import math_operations
+else:
+    from . lib.simple_package import math_operations
+    
 
 @click.command()
 @click.option('--name', prompt='Your name',
@@ -13,7 +17,7 @@ import click
               help='Your superpower.', default='invisibility')
 def main(name, title, superpower):
     click.echo(f'Hello, {title} {name} with your superpower as {superpower}!')
-    # print(math_operations.add(5,3))
+    print(math_operations.add(5,3))
     print(__name__)
 
 if __name__ == '__main__':
