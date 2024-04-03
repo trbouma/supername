db2b0c43164ffe81d193efc3e1bb117be96d07ab
trbouma <trbouma@gmail.com>
2024-04-03 09:40:44 -0400

    add argument

--- supername/main.py
+++ supername/main.py
@@ -3,11 +3,9 @@
 import typer
 
 
+def main(name: str):
+    print(f"Hello {name}")
 
-
-def main():
-    print("Hello World Supername, This is the edited file")
-    
 
 if __name__ == "__main__":
     typer.run(main)
