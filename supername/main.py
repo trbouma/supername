e65d8666f42e5c6e8505cb055e3e26d6bba16f47
trbouma <trbouma@gmail.com>
2024-04-03 11:55:15 -0400

    edits

--- supername/main.py
+++ supername/main.py
@@ -4,11 +4,13 @@ import click
 
 @click.command()
 @click.option('--name', prompt='Your name',
-              help='The person to greet.')
+              help='The person to greet.', default='Guest')
 @click.option('--title', prompt='Your title',
-              help='The title to greet.')
-def main(name, title):
-    click.echo(f'Hello, {title} {name}!')
+              help='The title to greet.', default='Honourable')
+@click.option('--superpower', prompt='Your superpower',
+              help='Your superpower.', default='invisibility')
+def main(name, title, superpower):
+    click.echo(f'Hello, {title} {name} with your superpower as {superpower}!')
 
 if __name__ == '__main__':
     main()
