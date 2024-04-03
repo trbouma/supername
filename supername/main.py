36ced3dc78c479f4b5ac9cc50ce928db94c8b5cf
trbouma <trbouma@gmail.com>
2024-04-03 11:02:57 -0400

    edit

--- supername/main.py
+++ supername/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 
-from lib.simple_package import math_operations
+
 
 def main():
     print("Hello World!")
-    print(math_operations.add(5,3))
+    
 
 
 if __name__ == "__main__":
