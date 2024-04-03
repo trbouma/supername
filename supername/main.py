1e246b69d908d7028e9b5bc2c4be1ee1c269e7d6
trbouma <trbouma@gmail.com>
2024-04-03 10:00:03 -0400

    update

--- supername/main.py
+++ supername/main.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
-import typer
+import argparse
 
+def main():
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description='A simple program with argparse')
 
-def main(name: str):
-    print(f"Hello {name}")
+    # Add arguments
+    parser.add_argument('--name', '-n', type=str, help='Your name')
+    parser.add_argument('--age', '-a', type=int, help='Your age')
 
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Access the values of the arguments
+    name = args.name
+    age = args.age
+
+    # Print out the values
+    print(f'Your name is: {name}')
+    print(f'Your age is: {age}')
 
 if __name__ == "__main__":
-    typer.run(main)
+    main()
