bac1d6f66f4aacdef53929a435656e33cb527372
trbouma <trbouma@gmail.com>
2024-04-03 11:37:33 -0400

    change to main

--- supername/main.py
+++ supername/main.py
@@ -5,8 +5,8 @@ import click
 @click.command()
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
-def hello(name):
+def main(name):
     click.echo(f'Hello, {name}!')
 
 if __name__ == '__main__':
-    hello()
+    main()
